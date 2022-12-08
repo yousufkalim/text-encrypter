0bbbf2149ad36245798b293877efb7d771f71cce
Yousuf Kalim <yousufkalim@outlook.com>
2022-12-08 19:10:44 +0500

    Wrote test cases

--- tests/index.test.js
+++ tests/index.test.js
@@ -1,12 +1,12 @@
 import { expect, test } from '@jest/globals';
-import defaultAwesomeFunction, { awesomeFunction } from '../src/index.js';
+import { encrypt, decrypt } from '../src/index.js';
 
-test('Should test default awesome function', () => {
-  const expectedVal = 'I am the Default Awesome Function, fellow comrade! - Yousuf';
-  expect(defaultAwesomeFunction('Yousuf')).toMatch(expectedVal);
+test('Check if plaintext to ciphertext conversion works', () => {
+  const value = 'hackerrank';
+  expect(encrypt(value, 5)).toEqual('mfhpjwwfsp');
 });
 
-test('Should test awesome function', () => {
-  const expectedVal = 'I am just an Awesome Function';
-  expect(awesomeFunction()).toMatch(expectedVal);
+test('Check if ciphertext to plaintext conversion works', () => {
+  const value = 'mfhpjwwfsp';
+  expect(decrypt(value, 5)).toEqual('hackerrank');
 });
